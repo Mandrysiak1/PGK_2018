f4cc630f079a332be227a218e396610da9d508e3
Adrian Fijałkowski <fijaladrian@gmail.com>
2018-10-18 12:30:13 +0000

    Table.cs edited online with Bitbucket

--- Assets/PGKScripts/Table.cs
+++ Assets/PGKScripts/Table.cs
@@ -9,30 +9,15 @@ using UnityEngine;
     {
         public string ID { get; private set; }
         private Order currOrder;
-        private bool imWaiting = false;
         public event PropertyChangedEventHandler PropertyChanged;
         private int beersOnTable = 0;
-
-        public bool TableAwaiting
-        {
-            get
-            {
-                return imWaiting;
-            }
-
-            set
-            {
-                this.imWaiting = value;
-                //PERHAPS THROW ORDER HERE TO UI HANDLER?
-                OnPropertyChanged("TableAwaiting");
-            }
-        }
+        
         public Table()
         {
             ID = System.Guid.NewGuid().ToString();
         }
 
-        public Order CurrOrder
+        public Order CurrentOrder
     {
         get
         {
@@ -48,8 +33,8 @@ using UnityEngine;
      
         public void NullifyOrder()
         {
-            //        this.SetOrder(null);
-
+            this.SetOrder(null);
+            OnPropertyChanged("CurrOrder");
         }
 
         public bool IsThereOrder()
