606c5379d6edd98e2b8f95422f4c44b2e7d6d76b
Adrian Fijałkowski <fijaladrian@gmail.com>
2018-10-19 16:31:13 +0200

    Camera fixed, so it doesnt rotate + minor changes in character movement

--- Assets/PGKScripts/CameraScript.cs
+++ Assets/PGKScripts/CameraScript.cs
@@ -6,26 +6,41 @@ public class CameraScript : MonoBehaviour {
 
     public Transform player;
 
-    public float smoothSpeed = 0.9f;
+    public float smoothSpeed = 0.3f;
 
-    public Vector3 cameraOffset = new Vector3(0, 12, 8);
-
+    public Vector3 cameraOffset = new Vector3(0, 8, 8);
+	public Vector3 rotationVector = new Vector3(45, 180, 0);
+    private Vector3 prevRotation;
     public Vector3 currentVelocity;
 
-    public float maxSpeed = 25;
+    public float maxSpeed = 4.5f;
 
 	// Use this for initialization
 	void Start () {
-
-	}
+		transform.Rotate(rotationVector.x, rotationVector.y, rotationVector.z);
+        prevRotation = new Vector3(rotationVector.x, rotationVector.y, rotationVector.z);
+    }
 	
 	// Update is called once per frame
 	void LateUpdate () {
         Vector3 nextPosition = player.position + cameraOffset;
+		/*
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, nextPosition, 
             ref currentVelocity, smoothSpeed, maxSpeed);
+			
         transform.position = smoothedPosition;
-        transform.LookAt(player);
+		*/
+        if(!rotationVector.Equals(prevRotation))
+        {
+            transform.rotation = Quaternion.Euler(rotationVector);
+            prevRotation.x = rotationVector.x;
+            prevRotation.y = rotationVector.y;
+            prevRotation.z = rotationVector.z;
+        }
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, nextPosition,
+            ref currentVelocity, smoothSpeed, maxSpeed);
+
+        transform.position = smoothedPosition;
     }
     private void Update()
     {
