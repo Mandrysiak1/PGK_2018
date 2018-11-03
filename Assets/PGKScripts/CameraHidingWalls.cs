f5314a28e8fc8f8135c3a9a299324578dc1b53f4
Łukasz Ciężki <rufus.lupus@outlook.com>
2018-11-03 17:56:40 +0100

    Add GameObject opacity decrease when it covers player

---
+++ Assets/PGKScripts/CameraHidingWalls.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CameraHidingWalls : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject Player;
+    [SerializeField]
+    private float Opacity = 0.1f;
+
+    private List<GameObject> lastCollidingObjects = new List<GameObject>();
+
+	void Update ()
+    {
+        RaycastHit[] hits;
+        Vector3 position = this.transform.position;
+        Vector3 direction = Player.transform.position - position;
+        hits = Physics.RaycastAll(position, direction);
+
+        List<GameObject> currentCollidingObjects = new List<GameObject>();
+        foreach(RaycastHit hit in hits)
+        {
+            if (hit.collider != null)
+            {
+                Collider collider = hit.collider;
+                GameObject collidedObject = collider.gameObject;
+
+                if (!currentCollidingObjects.Contains(collidedObject))
+                {
+                    if (collidedObject != Player)
+                    {
+                        SetTransparent(collidedObject);
+                        currentCollidingObjects.Add(collidedObject);
+                    }
+                }
+            }
+        }
+        List<GameObject> notCollidingObjects = lastCollidingObjects.Except(currentCollidingObjects).ToList();
+        foreach(GameObject nonCollidingObject in notCollidingObjects)
+        {
+            SetTransparent(nonCollidingObject, false);
+        }
+        lastCollidingObjects = currentCollidingObjects;
+	}
+    void SetTransparent(GameObject gameObject, bool transparent = true)
+    {
+        if(gameObject != null)
+        {
+            Renderer renderer = gameObject.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                if (transparent)
+                    renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, Opacity);
+                else
+                    renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 1f);
+            }
+        }
+    }
+}
