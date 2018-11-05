35310fd5fb779ce8e8440fb8f9672630f841bf8f
Adrian Fijałkowski <fijaladrian@gmail.com>
2018-11-05 21:32:00 +0100

    win streak ui, forgotten

---
+++ Assets/PGKScripts/Perks/WinStreak/WinStreakUI.cs
@@ -0,0 +1,24 @@
+﻿using Assets.PGKScripts.Interfaces;
+using TMPro;
+using UnityEngine;
+
+public class WinStreakUI : MonoBehaviour {
+
+    IWinStreakSource winStreakSource;
+    public TextMeshProUGUI counter;
+	// Use this for initialization
+	void Start () {
+        winStreakSource = FindObjectOfType<MainScript>();
+        winStreakSource.WinStreakChanged.AddListener(textChanger);
+	}
+
+    private void textChanger(int arg0, int arg1)
+    {
+        this.counter.text = arg1.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
