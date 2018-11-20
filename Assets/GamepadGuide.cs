ecfbdd29bfd3ed65a82a3348dec338f0a6d6e507
Łukasz Ciężki <rufus.lupus@outlook.com>
2018-11-20 10:57:11 +0100

    MainMenu: Add gamepad guide

---
+++ Assets/GamepadGuide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamepadGuide : ButtonVisualization
+{
+    private Vector3 initScale;
+
+    private void Start()
+    {
+        initScale = gameObject.transform.localScale;
+    }
+
+    protected override void Refresh()
+    {
+        if(GamepadMode)
+        {
+            gameObject.transform.localScale = initScale;
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(0f, 0f, 0f);
+        }
+    }
+}
