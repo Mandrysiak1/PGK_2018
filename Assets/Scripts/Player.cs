fce1336aa5f637ccda889babf2b719c8e10a5b80
Woland <me@woland.xyz>
2018-11-24 13:21:40 +0100

    Player: Remove unused members, internal to public

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,25 +6,24 @@ namespace Assets.PGKScripts
     public class Player
     {
         public int BeersHandedOut { get; private set; }
-        public int maxOrderSizeModifier { get; set; }
-
         public bool Vulnerable { get; set; }
 
-
         private PlayerPlate Plate;
+
         public Player(PlayerPlate plate)
         {
             if (plate == null)
                 Debug.Log("NULL");
             Plate = plate;
             BeersHandedOut = 0;
-            maxOrderSizeModifier = 0;
             Vulnerable = UpgradeClass.Vulnerable;
         }
-        internal void ResetBeersHandedOut()
+
+        public void ResetBeersHandedOut()
         {
             this.BeersHandedOut = 0;
         }
+
         public void RemoveBeer(OrderItem x)
         {
             Plate.RemoveItem(x);
@@ -33,18 +32,11 @@ namespace Assets.PGKScripts
 
         }
 
-        public void RemoveRandomItemFromPlate()
-        {
-            Plate.RemoveRandomItem();
-        }
         public void AddOrderItemOnPlate(OrderItem x)
         {
             Plate.AddItem(x);
-
             Debug.Log("Na tacy jest : " + Plate.GetItemQuantityOnPlate(x) + " " + x.name);
-
         }
-
 
         public int GetItemOrderOnPlateQuantity(OrderItem x)
         {
