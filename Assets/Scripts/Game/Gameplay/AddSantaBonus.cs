7d420301ce5b992d05ca4d21abf677963441c78f
DESKTOP-ORIC8DN\NASA <andrysiakmaciejj@gmail.com>
2018-12-03 21:50:37 +0100

    some spaghetti, i like spaghetti, bolognese>napoli btw

---
+++ Assets/Scripts/Game/Gameplay/AddSantaBonus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSantaBonus : MonoBehaviour {
+
+    [SerializeField]
+    private OrderController OrderController;
+
+    [SerializeField]
+    private OrderSource OrderSource;
+
+    [SerializeField]
+    private List<OrderItem> OrderItems;
+ 
+	void Start () {
+
+        OrderController.OrderFilled.AddListener(OnOrderFilled);
+        
+	}
+
+    private void OnOrderFilled(OrderSource source, Order order)
+    {
+        
+        if(CheckIfSantaRequest(order.Item))
+        {
+
+        }
+
+    }
+
+    private bool CheckIfSantaRequest(OrderItem item)
+    {
+        if (!OrderItems.Contains(item))
+        {
+            return false;
+        }else
+        {
+            return true;
+        }
+        
+    }
+
+}
