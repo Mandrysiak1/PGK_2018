1bc73907b02f2f14d2d3d158361046f915d4c5ff
Łukasz Ciężki <rufus.lupus@outlook.com>
2018-12-16 22:10:24 +0100

    Add AnimatorOffsetSetter script

---
+++ Assets/Scripts/AnimatorOffsetSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorOffsetSetter : MonoBehaviour
+{
+    [SerializeField]
+    private float AnimationOffset = 0.0f;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        string name = animator.runtimeAnimatorController.animationClips[0].name;
+        float length = animator.runtimeAnimatorController.animationClips[0].length;
+        animator.Play(animator.GetCurrentAnimatorStateInfo(0).fullPathHash, 0, AnimationOffset / length);
+    }
+
+    void Update()
+    {
+
+    }
+}
