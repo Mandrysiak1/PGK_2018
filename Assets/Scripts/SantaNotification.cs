c0ccf3404ff99e0dbdb326a6d22154da896f434f
mandrysiak <andrysiakmaciejj@gmail.com>
2018-12-17 20:10:51 +0100

    level 3 changes

--- Assets/Scripts/SantaNotification.cs
+++ Assets/Scripts/SantaNotification.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SantaNotification : Notification {
 
-    public string Format = "chuj w duepe";
+    public string onSuccess = "";
+    public string onFail = "";
 
     protected new void Start()
     {
         base.Start();
         var x = FindObjectOfType<AddSantaBonus>();
         x.OnSantaInfo.AddListener(OnActivate);
+        x.SantaFailure.AddListener(onSantaFailuer);
 
+    }
+
+    private void onSantaFailuer()
+    {
+        Show(string.Format(onFail));
     }
 
     private void OnActivate()
     {
-        Show(string.Format(Format));
+        Show(string.Format(onSuccess));
     }
 }
