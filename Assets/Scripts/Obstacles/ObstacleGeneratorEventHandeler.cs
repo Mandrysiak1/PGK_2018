076b40479323910e34e9edf953ce0a2f4dea6f3a
Łukasz Ciężki <rufus.lupus@outlook.com>
2018-12-17 19:39:20 +0100

    Modify obstacle generator to throw obstacles from sitting guests

--- Assets/Scripts/Obstacles/ObstacleGeneratorEventHandeler.cs
+++ Assets/Scripts/Obstacles/ObstacleGeneratorEventHandeler.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.PGKScripts;
 using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
@@ -11,10 +12,23 @@ public class ObstacleGeneratorEventHandeler : MonoBehaviour
     private int xMin, xMax, zMin, zMax;
     [SerializeField]
     private float yValue;
+    [SerializeField]
+    private List<GameObject> sittingGuests;
+    [SerializeField]
+    private float ThrowPower = 110.0f;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float ThrowPowerVariation = 0.25f;
+    [SerializeField]
+    private float ThrowYOffset = 0.5f;
+    [SerializeField]
+    private float ThrowYPowerMultiplier = 2.0f;
 
     private Scene currentScene;
     private Vector3 Position;
     private UnityEngine.Object[] prefabs;
+
+    private System.Random rand = new System.Random();
 
     void Start()
     {
@@ -54,23 +68,69 @@ public class ObstacleGeneratorEventHandeler : MonoBehaviour
 
     private void CalculatePosition()
     {
-        System.Random rand = new System.Random();
+        
+
+        GameObject sittingGuest = sittingGuests[rand.Next(0, sittingGuests.Count - 1)];
+
+        // TODO: move to other method
+        // trigger guest animator
+        Animator animator = sittingGuest.GetComponent<Animator>();
+        if (animator != null)
+            animator.SetTrigger("ThrowTrigger");
+
+
+        // get position for new obstacle
+        Bounds bounds = GetTotalMeshFilterBounds(sittingGuest.transform);
+        float height = ThrowYOffset;
+        Vector3 sittingGuestTopPosition = sittingGuest.transform.position + new Vector3(0, height, 0);
 
         int x = rand.Next(xMin, xMax);
-
         int z = rand.Next(zMin,zMax);
-        Position = new Vector3(x, yValue, z);
-        NavMeshHit hit;
-        NavMesh.SamplePosition(Position, out hit, 10, -1);
-        Position = hit.position;
-        Position.y = yValue;
+
+        Position = sittingGuestTopPosition;// new Vector3(x, yValue, z);
+        //NavMeshHit hit;
+        //NavMesh.SamplePosition(Position, out hit, 10, -1);
+        //Position = hit.position;
+        //Position.y = yValue;
     }
 
     private void SpawnOnObject()
     {
         int whatToSpawn = UnityEngine.Random.Range(0, prefabs.Length);
 
-        Instantiate(prefabs[whatToSpawn], Position, Quaternion.Euler(0, 32.028f, 0));
+        UnityEngine.Object spawned = Instantiate(prefabs[whatToSpawn], Position, Quaternion.Euler(0, 32.028f, 0));
+        GameObject gameObject = spawned as GameObject;
+        if(gameObject != null)
+        {
+            Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+            rigidbody.AddForce(new Vector3(
+                (((float)rand.NextDouble() - 0.5f) * 2 * ThrowPowerVariation) * ThrowPower,
+                (((float)rand.NextDouble() - 0.5f) * 2 * ThrowPowerVariation) * ThrowPower,
+                ((float)rand.NextDouble() * ThrowPowerVariation + 1) * ThrowPower * ThrowYPowerMultiplier
+                ));
+        }
+    }
 
+    private Bounds GetTotalMeshFilterBounds(Transform objectTransform)
+    {
+        var meshFilter = objectTransform.GetComponent<MeshFilter>();
+        var result = meshFilter != null ? meshFilter.mesh.bounds : new Bounds();
+
+        foreach (Transform transform in objectTransform)
+        {
+            var bounds = GetTotalMeshFilterBounds(transform);
+            result.Encapsulate(bounds.min);
+            result.Encapsulate(bounds.max);
+        }
+
+        //var scaledMin = result.min;
+        //scaledMin.Scale(objectTransform.localScale);
+        //result.min = scaledMin;
+
+        //var scaledMax = result.max;
+        //scaledMax.Scale(objectTransform.localScale);
+        //result.max = scaledMax;
+
+        return result;
     }
 }
