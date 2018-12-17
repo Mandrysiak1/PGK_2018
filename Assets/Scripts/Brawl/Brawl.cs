18919cb99b8903627c158d35b42b97e0d43a2f61
Łukasz Ciężki <rufus.lupus@outlook.com>
2018-12-17 23:54:57 +0100

    Level1: Set brawl

--- Assets/Scripts/Brawl/Brawl.cs
+++ Assets/Scripts/Brawl/Brawl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Brawl : MonoBehaviour
 {
     [SerializeField]
-    private List<GameObject> Variants;
+    private GameObject Variants;
 
     [SerializeField]
     private Transform CameraPosition;
@@ -12,21 +12,12 @@ public class Brawl : MonoBehaviour
     public string[] Insults;
     public AudioClip StartSound;
 
-    private void Start()
-    {
-        DisableAll();
-    }
 
     public void Setup(Camera camera)
     {
         camera.transform.position = CameraPosition.position;
         camera.transform.localRotation = CameraPosition.localRotation;
 
-        Variants.Random().gameObject.SetActive(true);
-    }
-
-    private void DisableAll()
-    {
-        Variants.ForEach(g => g.gameObject.SetActive(false));
+        Variants.SetActive(true);
     }
 }
