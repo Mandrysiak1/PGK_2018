812362f01cbf75f5c56dce271ce76b07a185504c
Adrianna <adriannabachulska@gmail.com>
2018-12-18 12:36:20 +0100

    tgfcgbm,

--- Assets/UIMainTut.cs
+++ Assets/UIMainTut.cs
@@ -27,11 +27,16 @@ public class UIMainTut : MonoBehaviour
     public Button Restart;
     public Button MainMenu;
     public Canvas NextLvlCanv;
+    public Button NextLevel;
+    public Button GoToShop;
+    public Button Continue;
     public Canvas PauseCanvas;
     public Canvas EndGameMenuCanvas;
     public Canvas FailureCanvas;
     public Canvas SuccesCanvas;
     public GameObject MenuConatiner;
+    public ScoreSystem scoreSystem;
+    public OrderGenerator orderGenerator;
 
     public MenuActivation menuActiveStatusEvent = new MenuActivation();
 
@@ -83,7 +88,13 @@ public class UIMainTut : MonoBehaviour
 
     private void SetButtons(bool init)
     {
+
+
+
+            NextLevel.gameObject.SetActive(false);
+            GoToShop.gameObject.SetActive(false);
             EventSystem.current.SetSelectedGameObject(Restart.gameObject);
+        
     }
 
     private void MenuActivationListener(bool arg0)
@@ -92,9 +103,11 @@ public class UIMainTut : MonoBehaviour
         {
             case true:
                 this.MenuConatiner.SetActive(true);
+                orderGenerator.enabled = false;
                 break;
             case false:
                 this.MenuConatiner.SetActive(false);
+                orderGenerator.enabled = true;
                 break;
         }
     }
@@ -109,16 +122,18 @@ public class UIMainTut : MonoBehaviour
     {
         if (arg1 == GameState.Success || arg1 == GameState.Failure)
         {
-            MenuActivated = true;
+            if (arg1 == GameState.Success)
+                MenuActivated = true;
             EventSystem.current.SetSelectedGameObject(Restart.gameObject);
-            EndGameText.text = "you " + (arg1 == GameState.Success ? "win" : "lose");
+            //EndGameText.text = "you " + (arg1 == GameState.Success ? "win" : "lose");
+            scoreSystem.ResetScore();
             mainScript.ResetBeersHandedOut();
 
             EndGameCanvas.enabled = true;
             //if (SceneManager.GetActiveScene().buildIndex == 1)
             //{
             if (arg1 == GameState.Success)
-            {
+            {   
                 NextLvlCanv.enabled = true;
                 SuccesCanvas.enabled = true;
             }
@@ -201,12 +216,40 @@ public class UIMainTut : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
+    void LoadNextLvl()
+    {
+        Time.timeScale = 1;
+        mainScript.CurrentGameState = GameState.Playing;
+    }
     private void ContinueGame()
     {
         Time.timeScale = 1;
 
     }
-    
+    void LoadShop()
+    {
+        UpgradeClass.endgamecanvas = EndGameCanvas.enabled;
+        UpgradeClass.nextlvlcanvas = NextLvlCanv.enabled;
+        //EndGameCanvas.gameObject.SetActive(false);
+        NextLvlCanv.enabled = false;
+        MainCanvas.enabled = false;
+        GameObject Customers = GameObject.Find("Customers");
+        if (Customers != null)
+            Customers.SetActive(false);
+        GameObject Obstacles = GameObject.Find("Obstacles");
+
+        if (Obstacles != null)
+        {
+            Obstacles.SetActive(false);
+            Obstacles.GetComponent<ObstacleGenerator>().CancelInvoke();
+        }
+        MenuActivated = false;
+
+        Time.timeScale = 1;
+
+        SceneManager.LoadSceneAsync("Shop", LoadSceneMode.Additive);
+        _inShop = true;
+    }
     private bool IsInList(GameObject go)
     {
         foreach (var el in buttonsSelectableList)
