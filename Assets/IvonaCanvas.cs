2523af6f890110c512f92c6259805a1c8ca9f838
Adrianna <adriannabachulska@gmail.com>
2018-12-30 19:27:14 +0100

    it is now possible to skip dialogues, other ivona scripts are not needed rn (every level uses basic IvonaCanvas script)

--- Assets/IvonaCanvas.cs
+++ Assets/IvonaCanvas.cs
@@ -1,58 +1,84 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 
 public class IvonaCanvas : MonoBehaviour {
     float time = 0f;
+    [SerializeField]
+    private List<string> Messages;
+    private int correntMessage = 0;
     public Image background;
+    public Image black;
     public Image XboxKey;
-    public Text bullshitspewingoutofcharactersmouthholyshitbarbecue;
-    public Image charactersdirtyassfacewhatthehellwhyarethesevariablenamessolong;
+    public Text textField;
+    public Image image;
     public TextMeshProUGUI E;
     string message;
     public float letterPaused = 0.1f;
+    private Canvas ui;
     bool flag;
+    Coroutine coroutine1;
     // Use this for initialization
     void Start()
     {
         background.enabled = false;
         XboxKey.enabled = false;
         E.enabled = true;
-        charactersdirtyassfacewhatthehellwhyarethesevariablenamessolong.enabled = false;
-        bullshitspewingoutofcharactersmouthholyshitbarbecue.enabled = false;
+        image.enabled = false;
+        textField.enabled = false;
+
+
 
     }
 
     void Update()
     {
 
+        if(ui == null)
+            ui = GameObject.Find("MainCanvas").GetComponent<Canvas>();
+
         if (waiths() && flag == false)
         {
             Time.timeScale = 0;
             flag = true;
             //Message will display will be at Text
-            message = bullshitspewingoutofcharactersmouthholyshitbarbecue.text;
+            message = Messages[0];
             //Set the text to be blank first
-            bullshitspewingoutofcharactersmouthholyshitbarbecue.text = "";
+            textField.text = "";
             background.enabled = true;
             XboxKey.enabled = true;
-            charactersdirtyassfacewhatthehellwhyarethesevariablenamessolong.enabled = true;
-            bullshitspewingoutofcharactersmouthholyshitbarbecue.enabled = true;
+            image.enabled = true;
+            textField.enabled = true;
+            black.enabled = true;
+            ui.enabled = false;
             //Call the function and expect yield to return
-            StartCoroutine(TypeText());
+            coroutine1 = StartCoroutine(TypeText());
+
         }
         if (Input.GetButtonDown("Submit") && flag == true) 
         {
-            if(message == bullshitspewingoutofcharactersmouthholyshitbarbecue.text)
+            correntMessage++;
+            if (Input.GetButtonDown("Submit") && flag == true && correntMessage == Messages.Count)
             {
-                //StopCoroutine(TypeText());
                 background.enabled = false;
+                black.enabled = false;
                 XboxKey.enabled = false;
                 E.enabled = false;
-                charactersdirtyassfacewhatthehellwhyarethesevariablenamessolong.enabled = false;
-                bullshitspewingoutofcharactersmouthholyshitbarbecue.enabled = false;
+                ui.enabled = true;
+                image.enabled = false;
+                textField.enabled = false;
                 Time.timeScale = 1;
+            }else 
+            if (correntMessage <= Messages.Count)
+            {
+                StopCoroutine(coroutine1);
+                textField.text = "";
+                message = Messages[correntMessage];
+                coroutine1 = StartCoroutine(TypeText());
+
+
             }
         }
     }
@@ -63,7 +89,7 @@ public class IvonaCanvas : MonoBehaviour {
         foreach (char letter in message.ToCharArray())
         {
             //Add 1 letter each
-            bullshitspewingoutofcharactersmouthholyshitbarbecue.text += letter;
+            textField.text += letter;
             yield return 0;
             yield return new WaitForSecondsRealtime(letterPaused);
         }
