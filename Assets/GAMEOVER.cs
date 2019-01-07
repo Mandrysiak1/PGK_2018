fadf334eba2f351e11e348e053398c8ab00fef84
mandrysiak <andrysiakmaciejj@gmail.com>
2019-01-07 23:47:55 +0100

    Level 5

---
+++ Assets/GAMEOVER.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAMEOVER : MonoBehaviour {
+
+    private MainScript main;
+	// Use this for initialization
+	void Start () {
+        main = FindObjectOfType<MainScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("asd fpd;sgn fkpadfngd");
+            main.GameOver();
+        }
+    }
+}
