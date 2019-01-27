f7423b15deebe4d6b777e140474145a12fc1e340
Woland <me@woland.xyz>
2019-01-27 18:06:20 +0100

    Fortnit background in main menu

--- Assets/Scripts/WanderingAI/WanderingText.cs
+++ Assets/Scripts/WanderingAI/WanderingText.cs
@@ -8,7 +8,7 @@ public class WanderingText : MonoBehaviour {
     public Text text;
 
 	void Update () {
-        if(text.enabled == true)
+        if(text != null && text.enabled == true)
         {
             Vector3 textPos = Camera.main.WorldToScreenPoint(this.transform.position);
             text.transform.position = textPos;
