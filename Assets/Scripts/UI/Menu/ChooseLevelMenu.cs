47ed345263cad29c35c7107dac0cd11653003315
Woland <me@woland.xyz>
2019-01-27 16:42:09 +0100

    Main menu code cleanup

---
+++ Assets/Scripts/UI/Menu/ChooseLevelMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseLevelMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameFlowController GameFlow;
+    [SerializeField]
+    private MenuContent Content;
+    [SerializeField]
+    private Button ButtonPrefab;
+
+    private void Awake()
+    {
+        foreach (GameLevel level in GameFlow.Flow.AllLevels.Reverse())
+        {
+            GameObject obj = Instantiate(ButtonPrefab.gameObject, transform);
+            Button button = obj.GetComponent<Button>();
+            TextMeshProUGUI text = obj.GetComponentInChildren<TextMeshProUGUI>();
+
+            text.text = level.Name.ToLower();
+            button.onClick.AddListener(() => LoadLevel(level));
+            obj.transform.SetAsFirstSibling();
+            Content.FirstSelection = obj;
+        }
+    }
+
+    private void LoadLevel(GameLevel level)
+    {
+        GameFlow.StartLevel(level);
+    }
+}
---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameFlowController Flow;
+
+    public void PlayGame()
+    {
+        StartFirstLevel();
+    }
+
+    private void StartFirstLevel()
+    {
+        Flow.StartFirstLevel();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
