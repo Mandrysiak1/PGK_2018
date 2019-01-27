553b2011eba7b187588d527273a385d21e4a2f2f
Woland <me@woland.xyz>
2019-01-27 23:57:39 +0100

    GamepadFirstSelection

---
+++ Assets/Scripts/Input/GamepadFirstSelection.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GamepadFirstSelection : MonoBehaviour
+{
+    public EventSystem EventSystem;
+
+    private void Start()
+    {
+        if (GamepadHelper.IsGamepadPresent())
+        {
+            if (EventSystem == null)
+                EventSystem = FindObjectOfType<EventSystem>();
+            EventSystem.SetSelectedGameObject(gameObject);
+        }
+    }
+}
