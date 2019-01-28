9240779a3136873e0795bb196fd58140e0466528
Woland <me@woland.xyz>
2019-01-28 12:45:52 +0100

    MainMenu: Credits

---
+++ Assets/Autoscroll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Autoscroll : MonoBehaviour
+{
+    public float Speed = 1.0f;
+    public float Delay = 0.0f;
+    [SerializeField]
+    private ScrollRect Scroll;
+
+    private float StartTime;
+
+    private void OnEnable()
+    {
+        StartTime = Time.time;
+        Scroll.verticalNormalizedPosition = 1.0f;
+    }
+
+    private void Update()
+    {
+        if (Time.time > StartTime + Delay)
+        {
+            if(Scroll.verticalNormalizedPosition > 0.00001f)
+                Scroll.verticalNormalizedPosition -= Speed * Time.deltaTime;
+        }
+    }
+}
